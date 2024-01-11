e56aeada99856ad4e00a13fba3e4010fca44d941
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2024-01-11 10:17:45 +0300

    update

--- client/src/pages/Hizmetlerimiz.jsx
+++ client/src/pages/Hizmetlerimiz.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import SayfaUstBilgi from '../components/SayfaUstBilgi/SayfaUstBilgi'
 import Hizmetler from '../components/Hizmetler/Hizmetler'
+import Gorseller from '../components/Gorseller/Gorseller'
 
 const Hizmetlerimiz = () => {
   return (
     <>
     <SayfaUstBilgi SayfaAdi="HİZMETLERİMİZ"/>
+    
     <Hizmetler/>
+    <Gorseller/>
     </>
   )
 }
