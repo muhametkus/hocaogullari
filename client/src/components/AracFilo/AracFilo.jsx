f01e7fd4e26373bdd64415e4e6354267a2ec00ba
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2024-02-07 22:34:09 +0300

    updatee

--- client/src/components/AracFilo/AracFilo.jsx
+++ client/src/components/AracFilo/AracFilo.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 
 const AracFilo = () => {
+    let FiloGaleri = [
+        { resimAdi: "Araç İçi", resimLink:"https://www.buskirala.com/wp-content/uploads/2022/01/27-kisilik-kiralik-minibus.jpg" },
+        { resimAdi: "Araç İçi", resimLink:"https://ekonomiyontem.com.tr/images/haberler/mercedes-benz-travego-10-yilini-travego-s-ile-kutluyor.jpg" },
+        { resimAdi: "Araç İçi", resimLink:"https://resim.epey.com/109793/m_2018-mercedes-sprinter-servis-316-cdi-2-2-163-bg-comfort-uzun-16plus1-1.jpg" },
+      ];
+
   return (
     <div className='w-[90%] mx-auto'>
         <div className='w-full'>
             <h1 className='text-center text-8xl md:text-7xl sm:text-[50px] font-extralight text-slate-800 mt-10 uppercase pb-8'>ARAÇ FİLOMUZ</h1>
         </div>
 
-        <div className='w-full flex flex-wrap justify-evenly'>
-            <img className='py-4 px-7 rounded-3xl shadow-2xl bg-white h-[250px]' src="https://www.buskirala.com/wp-content/uploads/2022/01/27-kisilik-kiralik-minibus.jpg" alt="" />
+        <div className='w-full flex flex-wrap justify-evenly sm:hidden'>
+        {FiloGaleri.map((resim) => (
+            <img className='py-4 px-7 rounded-3xl shadow-2xl bg-white h-[250px]' src={resim.resimLink} alt="" />
+        ))}
+        </div>
 
-            <img className='py-4 px-7 rounded-3xl shadow-2xl bg-white h-[250px]' src="https://ekonomiyontem.com.tr/images/haberler/mercedes-benz-travego-10-yilini-travego-s-ile-kutluyor.jpg" alt="" />
-
-            <img className='py-4 px-7 rounded-3xl shadow-2xl bg-white h-[250px]' src="https://resim.epey.com/109793/m_2018-mercedes-sprinter-servis-316-cdi-2-2-163-bg-comfort-uzun-16plus1-1.jpg" alt="" />
-
+        <div className='w-full hidden sm:flex flex-col gap-y-8'>
+        {FiloGaleri.map((resim) => (
+            <img className='py-4 px-7 rounded-3xl shadow-2xl bg-white w-[90%] mx-auto' src={resim.resimLink} alt="" />
+        ))}
         </div>
 
     </div>
