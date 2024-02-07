173c7518d8c188413416f83e83a2eafeea6b8cd2
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2024-02-07 23:05:01 +0300

    update

--- client/src/pages/Anasayfa.jsx
+++ client/src/pages/Anasayfa.jsx
@@ -9,9 +9,10 @@ const Anasayfa = () => {
   return (
     <>
      <div className='w-full sm:mt-[70px]'><Slider/></div>
+     <div className='w-full'><AracFilo/></div>
      <div className='w-full'><h2 className='text-center text-8xl md:text-7xl sm:text-[50px] font-extralight text-slate-800 mt-10 uppercase'>HİZMETLERİMİZ</h2></div>
      <Hizmetler/>
-     <div className='w-full'><AracFilo/></div>
+     
      <div className='w-full '><h2 className='text-center text-8xl md:text-7xl sm:text-[55px] font-extralight text-slate-800 mt-10 mb-5 uppercase'>Görseller</h2></div>
      <Gorseller/>
      <IletisimComp/>
